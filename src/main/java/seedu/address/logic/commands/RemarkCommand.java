d82ac98415126b615556b9f5f722786bbe925c38
Labayna Neil Brian Narido <neilbrian.nl@gmail.com>
2019-09-08 12:15:04 +0800

    Before adding remark

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -7,6 +10,9 @@ import seedu.address.model.Model;
  * Changes the remark of an existing person in the address book.
  */
 public class RemarkCommand extends Command {
+
+    private final Index index;
+    private final String remark;
 
     public static final String COMMAND_WORD = "remark";
 
@@ -18,10 +24,20 @@ public class RemarkCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
 
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
+    /**
+     * @param index of the person in the filtered person list to edit the remark
+     * @param remark of the person to be updated to
+     */
+    public RemarkCommand(Index index, String remark) {
+        requireNonNull(index, remark);
+
+        this.index = index;
+        this.remark = remark;
+    }
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
     }
 }
